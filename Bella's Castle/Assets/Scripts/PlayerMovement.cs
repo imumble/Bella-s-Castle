0567039b1a8e0def01c771bfc70c9e71e0d2cde2
imumble <imumble70@yahoo.com>
2020-07-15 20:43:03 -0500

    First steps to greatness
    Elsa and Door added. Elsa needs to move forward and back. Not up and down through the floor.

---
+++ Bella's Castle/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float speed = 4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKey (KeyCode.A)) {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);    
+        }
+        else if (Input.GetKey (KeyCode.D)) {
+            transform.Translate(-Vector2.right * speed * Time.deltaTime);   
+        }
+        else if (Input.GetKey (KeyCode.W)) {
+            transform.Translate(Vector2.up * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey (KeyCode.S)) {
+            transform.Translate(-Vector2.up * speed * Time.deltaTime);  
+        }
+    }
+}

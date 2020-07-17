d0ddad56bd58451569f5fd86b02728e0f7f39d0e
TheSchlote <TheSchlote@gmail.com>
2020-07-17 01:04:10 -0500

    Added Player Movement and a new Room.
    Logan you can control Elsa and I even gave her gravity to keep her on the ground. I copied your StartingRoom and made the Main Room which is a bit bigger and I grouped all of your objects into GameObjects as children to move things around easier.

--- Bella's Castle/Assets/Scripts/DoorMechanic.cs
+++ Bella's Castle/Assets/Scripts/DoorMechanic.cs
@@ -1,18 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class DoorMechanic : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(2);
     }
 }

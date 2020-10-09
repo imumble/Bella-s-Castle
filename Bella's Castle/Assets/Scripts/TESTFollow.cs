d17f2007f23fc5d81a687a9b7170c228122cf4c5
imumble <imumble70@yahoo.com>
2020-10-09 12:30:07 -0500

    Fixed Support and Started the Follow Script
    I made the Support Character closer to the same size as the main character. I've been playing trying to get her to follow him but havent figured out fully how. Figured I would have you look at it. I followed this video https://www.youtube.com/watch?v=zssU0MZcIx8  They are both currently in the test scene

---
+++ Bella's Castle/Assets/Scripts/TESTFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TESTFollow : MonoBehaviour
+{
+    public GameObject Player;
+    public float TargetDistance;
+    public float AllowedDistance = 5;
+    public GameObject Partner;
+    public float FollowSpeed;
+    public RaycastHit Shot;
+    void Update()
+    {
+        if(Physics.Raycast(transform.position,transform.TransformDirection(Vector3.forward),out Shot))
+        {
+            TargetDistance = Shot.distance;
+            if(TargetDistance >= AllowedDistance)
+            {
+                FollowSpeed = 1f;
+                transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, FollowSpeed);
+            }
+            else
+            {FollowSpeed = 0;
+            }
+        }
+    }
+}

cb8a33c4d88e054bc0b1e03697837818d9c01345
TheSchlote <TheSchlote@gmail.com>
2020-10-10 11:11:38 -0500

    Started a follower script
    The follower still needs to animate, and flip directions, but thats a problem for future us hahaha

---
+++ Bella's Castle/Assets/Scripts/Overworld/CompanionFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompanionFollow : MonoBehaviour
+{
+    public GameObject player;
+    public float targetDistance;
+    public float allowedDistance = 6;
+    public GameObject follower;
+    public float followSpeed;
+
+    void Update()
+    {
+        targetDistance = Vector3.Distance(player.transform.position, follower.transform.position);
+        if (targetDistance >= allowedDistance)
+        {
+            followSpeed = 0.1f;
+            //maybe tell her to walk animate?
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, followSpeed);
+        }
+        else
+        {
+            followSpeed = 0f;
+            //maybe tell her to idle animate?
+        }
+    }
+}
